f4deb232c0f5aee2d9bc5947544859fa26367774
slezer <contact@slezer.cc>
2023-05-01 13:31:52 -0400

    v1.5

--- DiskTool.bat
+++ DiskTool.bat
@@ -1,7 +1,8 @@
 @echo off
-title DiskTool v1.4
-set VaultPath=Documents\
+title DiskTool v1.5
 set VaultName=Documents
+set password=YOURPASSWORD
+set VaultPath=%VaultName%\
 set aesKey=7g3f8j2w9
 set Authenticated=false
 set new=true
@@ -14,11 +15,11 @@ cls
 echo     ___  _     __  ______          __
 echo    / _ \(_)__ / /_/_  __/__  ___  / /
 echo   / // / (_- /   _// / / _ \/ _ \/ / 
-echo  /____/_/___/_/\_\/_/  \___/\___/_/  v1.4
+echo  /____/_/___/_/\_\/_/  \___/\___/_/  v1.5
 echo.
 echo Welcome to DiskTool, type "help" to view all commands.
 if %Authenticated%==true echo. && goto :sectorAsk
-if %new%==true echo %VaultPath% folder created, files stored inside this folder can now be encrypted.
+if %new%==true echo \%VaultPath% folder created, files stored inside this folder can now be encrypted.
 echo.
 :actionask
 set/p "action=> "
@@ -42,15 +43,12 @@ goto :done
 
 :InitForEncrypt
 if exist %VaultName%.7z echo Files are already encrypted. && goto :actionask
-echo Use layer 1 encryption (1) or 2 layer encryption (2)?
+echo Use 1 layer encryption or 2 layer encryption (1/2)?
 set/p "layer=> "
 if %layer%==1 goto :1layer
 if %layer%==2 goto :1layer
 echo Invalid choice.
 goto :InitForEncrypt
-
-:InitForConvert
-
 
 :1layer
 echo Encrypting \%VaultPath%, this may take a while.
@@ -71,8 +69,8 @@ if %Authenticated%==true goto :sectorAsk
 if %Authenticated%==false echo Enter your decryption password to decrypt %name%.
 :GetPass
 set /p "pass=> "
-if NOT %pass%==YOURPASSWORD echo Decryption password incorrect, please try again. && goto :GetPass
-if %pass%==YOURPASSWORD set Authenticated=true && goto :login
+if NOT %pass%==%password% echo Decryption password incorrect, please try again. && goto :GetPass
+if %pass%==%password% set Authenticated=true && goto :login
 
 :sectorAsk
 echo Decrypt specific folder? (y/n)
@@ -93,9 +91,9 @@ echo Decrypting %specPath%, this may take a while.
 goto :1layerdspec
 
 :nospec
-echo Extracting %Vaultname%.7z, this may take a while.
+echo Extracting \%Vaultname%.7z, this may take a while.
 if exist %VaultName%.7z 7z x %VaultName%.7z -pSsvdFxQ3N7Z7 >NUL && del /q %VaultName%.7z >NUL
-echo Decrypting %VaultPath%, this may take a while.
+echo Decrypting \%VaultPath%, this may take a while.
 
 :1layerd
 FOR /f "delims=" %%G IN ('dir /s /b /a-d %VaultPath%') DO (call :Decrypt "%%G")
@@ -126,16 +124,17 @@ echo Enter Key Name:
 set/p "KeyName=> "
 echo Enter Key Content:
 set/p "KeyContent=> "
-echo %KeyContent% > "%VaultPath%Keys\%KeyName%.txt" >NUL
+if NOT exist %VaultPath%Keys\ mkdir %VaultPath%Keys\
+echo %KeyContent% > "%VaultPath%Keys\%KeyName%.txt"
 echo Created %KeyName%.txt
 goto :actionask
 
 :Help
-echo encrypt - encrypts files stored in %VaultPath%.
-echo decrypt - decrypts all files stored in %VaultPath%.
-echo newkey - create a new text key in %VaultPath%Keys\.
+echo encrypt - encrypts files stored in \%VaultPath%.
+echo decrypt - decrypts all files stored in \%VaultPath%.
+echo newkey - create a new text file in \%VaultPath%Keys\.
 echo help - displays this text.
 echo clear, cls - clears the console.
 echo done - delete decrypted files.
-echo exit - closes the DiskTool console.
+echo exit - closes DiskTool.
 goto :actionask
